80f0698d61b68246565b5df77f80dedf5da94910
sergio <sergiosusin.ssc@gmail.com>
2021-02-08 10:57:00 +0100

    Registro persona
    Registro persona con update

--- GestionPapeleoFCT-Back/app/Http/Controllers/API/AuthController.php
+++ GestionPapeleoFCT-Back/app/Http/Controllers/API/AuthController.php
@@ -23,18 +23,32 @@ class AuthController extends Controller {
         ]);
 
         $validatedData['password'] = \Hash::make($request->input("password"));
+
         $user = User::create($validatedData);
         $user->roles()->attach($request->input("rol"));
+
         $accessToken = $user->createToken('authToken')->accessToken;
+
         return response()->json(['message' => ['user' => $user, 'access_token' => $accessToken], 'code' => 201], 201);
     }
 
+    /**
+     * Registro profesor dirigido a la tabla persona
+     * @param Request $request
+     * @return json
+     */
     public function register_persona(Request $request) {
-
         if (Persona::where('correo', $request->input('email'))->count() == 1) {
-            return response()->json(['message' => 'Registro incorrecto. Revise las credenciales.', 'code' => 400], 400);
+            $persona = Persona::where('correo', $request->input('email'))->first();
+            $persona->dni = $request->input("dni");
+            $persona->nombre = $request->input("nombre");
+            $persona->apellidos = $request->input("apellidos");
+            $persona->localidad = $request->input("localidad");
+            $persona->residencia = $request->input("residencia");
+            $persona->tlf = $request->input("tlf");
+            $persona->save();
+            return response()->json(['message' => ['user' => $persona], 'code' => 201], 201);
         }
-
         $validatedData = [
             'correo' => $request->input("email"),
             'dni' => $request->input("dni"),
@@ -44,10 +58,25 @@ class AuthController extends Controller {
             'residencia' => $request->input("residencia"),
             'tlf' => $request->input("tlf"),
         ];
-
         $persona = Persona::create($validatedData);
+
         return response()->json(['message' => ['user' => $persona], 'code' => 201], 201);
     }
+
+    /**
+     * funcion para comprobar si una persona existe, si existe devuelve los datos si no null.
+     * @param Request $request
+     * @return type
+     */
+    public function isPersona(Request $request) {
+        if (Persona::where('correo', $request->input('email'))->count() == 1) {
+            $persona = Persona::where('correo', $request->input('email'))->first();
+            return response()->json(['message' => ['persona' => $persona], 'code' => 201], 201);
+        } else {
+            return response()->json(['message' => ['persona' => null], 'code' => 201], 201);
+        }
+    }
+
     public function login(Request $request) {
         $loginData = $request->validate([
             'email' => 'email|required',
