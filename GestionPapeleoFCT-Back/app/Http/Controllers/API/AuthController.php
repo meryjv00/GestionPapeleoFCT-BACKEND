726f0c90bc5468a2e98c586e5d459ac06b669d36
Mery <maria.juan.vi@gmail.com>
2021-02-12 11:07:16 +0100

    Hotfix merge

--- GestionPapeleoFCT-Back/app/Http/Controllers/API/AuthController.php
+++ GestionPapeleoFCT-Back/app/Http/Controllers/API/AuthController.php
@@ -36,8 +36,8 @@ class AuthController extends Controller {
      * @return json
      */
     public function register_persona(Request $request) {
-        if (Persona::where('correo', $request->input('email'))->where('dni', $request->input('dni'))->count() == 1) {
-            $persona = Persona::where('correo', $request->input('email'))->first();
+        if (Persona::where('dni', $request->input('dni'))->count() == 1) {
+            $persona = Persona::where('dni', $request->input('dni'))->first();
             $persona->dni = $request->input("dni");
             $persona->nombre = $request->input("nombre");
             $persona->apellidos = $request->input("apellidos");
@@ -45,6 +45,11 @@ class AuthController extends Controller {
             $persona->residencia = $request->input("residencia");
             $persona->tlf = $request->input("tlf");
             $persona->save();
+            RolUsuario::create([
+                'role_id' => $request->input("rol"),
+                'user_dni' => $request->input("dni")
+            ]);
+
             return response()->json(['message' => ['user' => $persona], 'code' => 201], 201);
         }
         $validatedData = [
@@ -91,20 +96,27 @@ class AuthController extends Controller {
         }
 
         $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        //$user = User::where('email', '=', $request->input('email'))->get();
+        $user = \DB::table('users')
+                ->select('dni')
+                ->where('email','=',$request->input('email'))
+                ->get();
+        
         //Obtener todos los datos del usuario
-        $persona = Persona::where("correo", "=", $request->input('email'))->first();
+        $persona = Persona::where("dni", "=", $user[0]->dni)->first();
 
         //Obtener el rol del usuario
         $rol = RolUsuario::where("user_dni", "=", $persona->dni)->get();
         //return response()->json(['message' => ['rol' => $rol], 'code' => 200], 200);
 
-        if($rol[0]->role_id == 1) {
+        if ($rol[0]->role_id == 1) {
             $rolDescripcion = "Director";
-        }else if ($rol[1]->role_id == 2) {
+        } else if ($rol[1]->role_id == 2) {
             $rolDescripcion = "Jefe de estudios";
         } else if ($rol[1]->role_id == 3) {
             $rolDescripcion = "Tutor";
-        } 
+        }
         //return response(['user' => auth()->user(), 'access_token' => $accessToken]);
         return response()->json(['message' => ['user' => auth()->user(), 'access_token' => $accessToken, 'datos_user' => $persona, 'rol' => $rolDescripcion], 'code' => 200], 200);
     }
