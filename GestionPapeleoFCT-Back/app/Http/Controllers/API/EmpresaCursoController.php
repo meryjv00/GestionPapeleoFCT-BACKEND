58095a01c65a490e50952e041f0ace1d89289a12
Mery <maria.juan.vi@gmail.com>
2021-02-20 19:42:52 +0100

    Ajuste empresa curso, registro

--- GestionPapeleoFCT-Back/app/Http/Controllers/API/EmpresaCursoController.php
+++ GestionPapeleoFCT-Back/app/Http/Controllers/API/EmpresaCursoController.php
@@ -12,20 +12,20 @@ class EmpresaCursoController extends Controller
     // Método para añadir una nueva empresa a las practicas de un curso
     public function store(Request $request) {
         $empresaCurso = EmpresaCurso::create($request->all());
-        if (!$empresaCurso) {
+        if (count($empresaCurso) == 0) {
             return response()->json(['errors' => array(['code' => 404, 'message' => 'No se ha podido registrar la empresa' . $empresaCurso])], 404);
         }
         return response()->json(['code' => 201, 'message' => 'Datos insertados correctamente'], 201);
     }
 
     // Método para eliminar una empresa de las practicas de un curso
-    public function destroy($id){
-        $empresaCurso = EmpresaCurso::find($id);
-
-        if (!$empresaCurso) {
+    public function destroy($idEmpresa,$idCurso){
+        $empresaCurso = EmpresaCurso::where('idEmpresa','=',$idEmpresa)->where('idCurso','=',$idCurso)->get();
+        
+        if (count($empresaCurso) == 0) {
             return response()->json(['errors' => array(['code' => 404, 'message' => 'No se encuentra ninguna empresa asociada a ese curso'])], 404);
         }
-        $empresaCurso->delete();
+        $empresaCurso[0]->delete();
         return response()->json(['code' => 200, 'message' => 'La empresa ha sido eliminado del curso'], 200);
     }
 
