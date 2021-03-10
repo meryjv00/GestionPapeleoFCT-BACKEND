f19a4cb3495aaf3addc6e63cab64a95bda2023bb
Daniel Sánchez <danielsanchezc6@gmail.com>
2021-03-10 01:10:45 +0100

    Trabajando

--- GestionPapeleoFCT-Back/tests/Feature/RegistroTest.php
+++ GestionPapeleoFCT-Back/tests/Feature/RegistroTest.php
@@ -8,7 +8,7 @@ use Tests\TestCase;
 
 class RegistroTest extends TestCase
 {
-    
+    //Registro de usuario
     public function testRegistroValido() {
 
         $response = $this->postJson('/api/register', array('dni' => '00999000N', 'email' => '12300567890@a.com', 'password' => 'hola', 'activado' => 0));
@@ -17,6 +17,7 @@ class RegistroTest extends TestCase
         $response->assertStatus(201);
     }
     
+    //Error por correo repetido Middleware notUser
     public function testRegistroInvalido() {
         $response = $this->postJson('/api/register', array('dni' => '05999032N', 'email' => 'director@gmail.com', 'password' => 'hola', 'activado' => 0));
         //dump($response->getContent());
